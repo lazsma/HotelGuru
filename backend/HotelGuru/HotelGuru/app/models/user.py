342df97a60a36d15b8fd74ee2a3f170425ec67a3
Tamás Szerdahelyi <aaye57@gmail.com>
2026-03-30 18:15:58 +0200

    Modelek: user,role,address + init_db.py létrehozása
    Létrehoztam a user.py, role.py, init_db.py és address.py fájlokat.

---
+++ backend/HotelGuru/HotelGuru/app/models/user.py
@@ -0,0 +1,68 @@
+from app.extensions import db, Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from sqlalchemy.types import String, Integer
+from sqlalchemy import ForeignKey, Column, Table
+from typing import List, Optional
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+#több - több kapcsolat miatt kell ez a tabla
+UserRole = Table(
+    "userroles",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id")),
+    Column("role_id", ForeignKey("roles.id"))
+)
+
+#foglaláshoz -----------tábla és id neveket át kell írni
+
+# UserReservation = Table(
+#     "userreservations",
+#     Base.metadata,
+#     Column("user_id", ForeignKey("users.id")),
+#     Column("role_id", ForeignKey("roles.id"))
+# )
+
+class User(db.Model):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(String(30))
+    email: Mapped[Optional[str]]
+
+    szemely_igazolvany_szam : Mapped[int]
+
+    password: Mapped[str] = mapped_column(String(30))
+    phone : Mapped[str] = mapped_column(String(30))
+   
+
+    roles: Mapped[List["Role"]] = relationship(secondary=UserRole, back_populates="users")
+    
+
+
+    #------------ki melyik hotelben dolgozik....hotel user kapcsolat--------------------
+    #hotel_id : Mapped[Optional[int]] = mapped_column(ForeignKey("users.id"))
+    #hotel : Mapped[Optional["User"]] = relationship(back_populates="user",lazy=True) #lazy=True 
+   
+
+
+    #-----------------Foglalás----------------------
+
+
+
+
+    address_id: Mapped[int] = mapped_column(ForeignKey("addresses.id"))
+    address : Mapped["Address"] = relationship(back_populates="user", lazy=True)
+    
+
+    
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!s}, email={self.email!r})"
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
